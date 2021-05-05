d6867cb9d06f495ad26a3463bcc01254dc6712e4
Matt Mayfield <mattmayf411@gmail.com>
2021-05-04 20:02:10 -0500

    design

--- components/Layout.js
+++ components/Layout.js
@@ -8,10 +8,21 @@ import { token, FRONTEND_URI } from "../lib/spotifyHelper";
 export default function Layout({ children, profile }) {
   const router = useRouter();
 
+    // FIX FOR VH ON MOBILE
+    const changeVhVariable = () => {
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      const vh = typeof window !== 'undefined' && window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      typeof document !== 'undefined' &&
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+  
+
   useEffect(() => {
     if (!token) {
       !token && router.push(FRONTEND_URI);
-    }
+    } 
+    changeVhVariable();
   }, []);
 
   return (

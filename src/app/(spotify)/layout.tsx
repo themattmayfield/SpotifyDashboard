0fac64e2be2f1d1099775ae0ca4a253b1de8ff44
Matt Mayfield <mattmayf411@gmail.com>
2023-08-28 18:08:33 -0500

    update (#15)
    * update
    
    * update

--- src/app/(spotify)/layout.tsx
+++ src/app/(spotify)/layout.tsx
@@ -10,7 +10,7 @@ export default async function SpotifyLayout({ children }: TRootLayoutProps) {
   return (
     <div className="flex h-screen bg-transparent lg:pr-6 lg:pl-2">
       <SideNav />
-      <div className="flex flex-col min-h-screen overflow-x-hidden flex-grow">
+      <div className="flex flex-col min-h-screen overflow-x-hidden flex-grow no-scrollbar">
         <Nav />
         {children}
       </div>
--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -17,23 +17,27 @@ export default function Playlist({
       : 'lg:h-[25vw] lg:w-[25vw] 2xl:h-64 2xl:w-64'
   } cursor-pointer overflow-hidden bg-custom-darkgray bg-cover bg-center h-[45vw] w-[45vw] flex items-center justify-center transition duration-300 ease-in-out transform hover:scale-105`;
   return (
-    <div className="inline-block max-w-min mx-auto ">
-      <Link href={`/playlists/${playlist.id}`}>
-        {playlist.images.length ? (
-          <div
-            style={{
-              backgroundImage: `url(${playlist.images[0].url})`,
-            }}
-            className={imageClassess}
-          ></div>
-        ) : (
-          <div className={imageClassess}>
-            <IoMusicalNotesSharp className="text-white h-24 w-24 " />
-          </div>
-        )}
-      </Link>
-      <div className={`text-center mt-4 ${analytic && 'space-y-2'}`}>
-        <p className="text-white">{playlist.name}</p>
+    <div className="inline-block max-w-min mx-auto">
+      {playlist.images.length ? (
+        <Link
+          style={{
+            backgroundImage: `url(${playlist.images[0].url})`,
+          }}
+          className={imageClassess}
+          href={`/playlists/${playlist.id}`}
+        />
+      ) : (
+        <Link href={`/playlists/${playlist.id}`} className={imageClassess}>
+          <IoMusicalNotesSharp className="text-white h-24 w-24 " />
+        </Link>
+      )}
+
+      <div
+        className={`text-center mt-4 w-[45vw] lg:w-[25vw] 2xl:w-64 ${
+          analytic && 'space-y-2'
+        }`}
+      >
+        <p className="text-white truncate overflow-hidden">{playlist.name}</p>
         {analytic && (
           <p className="text-white">By {playlist.owner.display_name}</p>
         )}

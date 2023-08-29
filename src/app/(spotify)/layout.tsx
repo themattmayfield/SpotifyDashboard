2a78fa200849a9cefab72843eb20a4e3b8f0ff3c
Matt Mayfield <mattmayf411@gmail.com>
2023-08-29 12:08:06 -0500

    Idk (#18)
    * update
    
    * init

--- src/app/(spotify)/layout.tsx
+++ src/app/(spotify)/layout.tsx
@@ -8,7 +8,7 @@ type TRootLayoutProps = {
 
 export default async function SpotifyLayout({ children }: TRootLayoutProps) {
   return (
-    <div className="flex h-screen bg-transparent lg:pr-6 lg:pl-2">
+    <div className="flex h-screen bg-transparent lg:pr-6 lg:pl-2 no-scrollbar overflow-hidden">
       <SideNav />
       <div className="flex flex-col min-h-screen overflow-x-hidden flex-grow no-scrollbar">
         <Nav />
